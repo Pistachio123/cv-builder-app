042582caa238b6fb202195ae5281b5be321d913f
Pistachio123 <77029076+Pistachio123@users.noreply.github.com>
2023-06-13 10:52:37 +0300

    Second Commit

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -31,7 +31,7 @@ const Header = () => {
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <Container>
           <LinkContainer to='/'>
-            <Navbar.Brand>MERN Auth</Navbar.Brand>
+            <Navbar.Brand>CV Builder</Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
